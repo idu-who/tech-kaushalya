6cefd7948eb228378101c2a626edcfc48f50a7df
idu-who <104561260+idu-who@users.noreply.github.com>
2023-03-07 21:56:17 +0400

    footer
    Signed-off-by: idu-who <104561260+idu-who@users.noreply.github.com>

--- pages/views.py
+++ pages/views.py
@@ -5,10 +5,9 @@ from django.shortcuts import render
 
 def home_view(request):
     context = {}
-    # if request.GET:
-    #     pass
-    # else if request.POST:
-    #     pass
+    if request.POST:
+        email_input = request.POST.get('email')
+        print('signed up for updates:', email_input)
     return render(request, 'pages/home.html', context)
 
 
